912d730a66ed862417223d02fe17618624ef993a
zeezo679 <ziadabdalla105@gmail.com>
2026-04-17 01:02:13 +0200

    Final Clean Push

--- SoftBridge.Web/Extensions/ApplicationServicesExtensions.cs
+++ SoftBridge.Web/Extensions/ApplicationServicesExtensions.cs
@@ -7,16 +7,21 @@ using SoftBridge.Abstraction.IServices.Profiles;
 using SoftBridge.Abstraction.IServicesContract.Chat;
 using SoftBridge.Abstraction.IServicesContract.Notification;
 using SoftBridge.Abstraction.IServicesContract.Services;
+using SoftBridge.Abstraction.IServicesContract.Token;
+using SoftBridge.Domain.Contracts.UnitOfWorkPattern;
 using SoftBridge.Domain.Models.ServiceAggregates;
+using SoftBridge.Persistence.ImplementsContracts.UowImmlementation;
 using SoftBridge.Services.Resolver;
 using SoftBridge.Services.Services;
 using SoftBridge.Services.Services.Admin;
 using SoftBridge.Services.Services.AuthImplementation;
 using SoftBridge.Services.Services.CategoryImplementation;
 using SoftBridge.Services.Services.Chat;
+using SoftBridge.Services.Services.ClientImplementation;
 using SoftBridge.Services.Services.NotificationImplementation;
 using SoftBridge.Services.Services.NotificationImplementation.StrategyPattern;
 using SoftBridge.Services.Services.ServiceManagement;
+using SoftBridge.Services.Services.Token;
 using SoftBridge.Shared.Common.Dto.Notification.Settings;
 
 namespace SoftBridge.Web.Extensions
@@ -42,6 +47,15 @@ namespace SoftBridge.Web.Extensions
             services.AddScoped<IAdminService, AdminService>();
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddScoped<IChatService, ChatService>();
+            
+            //// 5. Client
+            services.AddScoped<IClientProfileService, ClientService>();
+
+//// 6. Provider 
+//services.AddScoped<IProviderProfileService, ServiceProviderService>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<ITokenService, TokenService>();
 
             // 4. transient services : because they are used in resolvers and we want a new instance each time
             // and the class is too small to be scoped or singleton it take string and return string so it is better to be transient
