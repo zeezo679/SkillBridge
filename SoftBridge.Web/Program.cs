2fc2627c687c3afa421a2c73018f173b85f6afdd
zeezo679 <ziadabdalla105@gmail.com>
2026-04-25 17:56:06 +0300

    tests

--- SoftBridge.Web/Program.cs
+++ SoftBridge.Web/Program.cs
@@ -1,4 +1,3 @@
-using PharmaBridge.Web.Extensions;
 using SoftBridge.Persistence.Extensions;
 using SoftBridge.Persistence.ProgramServices;
 using SoftBridge.Services.AutoMapper;
@@ -42,7 +41,10 @@ namespace SoftBridge.Web
             var app = builder.Build();
 
             // Initialization
-            await app.SeedDatabaseAsync();
+            if (!app.Environment.IsEnvironment("Testing"))
+            {
+                await app.SeedDatabaseAsync();
+            }
 
             // Configure the HTTP Request Pipeline
             app.UseMiddleware<GlobalErrorHandlerMiddleware>();
